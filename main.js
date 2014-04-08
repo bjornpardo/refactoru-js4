a5f07238ea9ea3852a9d12aa0a32f0ae9d17fcac
Bjorn Pardo <bjorn@xend.com.ph>
2014-04-08 17:31:22 -0600

    apr530

--- main.js
+++ main.js
@@ -1,12 +1,25 @@
-var vicName=[]
-var vicAdd=[]
-var vicPhone=[]
+var vicNames=[];
+var vicAdds=[];
+var vicPhones=[];
 
-var vicNum=prompt("How many victims would you like to enter?")
+var vicNum=+prompt("How many victims would you like to enter?")
 for (var i=0; i<vicNum; i++){
-	vicName.push(prompt("Please enter victim\'s"));
-	vicAdd.push(prompt("Please enter victim\'s address"));
-	vicPhone.push(prompt("Please enter victim\'s phone number"));
-	
+	vicNames.push(prompt("Please enter victim\'s name"));
+	vicAdds.push(prompt("Please enter victim\'s address"));
+	vicPhones.push(prompt("Please enter victim\'s phone number"));	
 }
 
+var volNames=[];
+var volAdds=[];
+var volPhones=[];
+
+	volNames.push(prompt("Please enter your name"));
+	volAdds.push(prompt("Please enter your address"));
+	volPhones.push(prompt("Please enter your phone number"));	
+
+var summary="Persons in Need: " + vicNames.length + "\n" +
+			"Number of Volunteers: " + volNames.length + "\n" +
+			"Victim Names: " + vicNames + "\n" +
+			"Volunter Names: " + volNames;
+
+alert(summary);
