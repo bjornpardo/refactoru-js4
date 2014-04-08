8f6d4b344f14667e54d8212056b830097b114277
Bjorn Pardo <bjorn@xend.com.ph>
2014-04-08 16:13:26 -0600

    apr8413

---
+++ main.js
@@ -0,0 +1,12 @@
+var vicName=[]
+var vicAdd=[]
+var vicPhone=[]
+
+var vicNum=prompt("How many victims would you like to enter?")
+for (var i=0; i<vicNum; i++){
+	vicName.push(prompt("Please enter victim\'s"));
+	vicAdd.push(prompt("Please enter victim\'s address"));
+	vicPhone.push(prompt("Please enter victim\'s phone number"));
+	
+}
+
